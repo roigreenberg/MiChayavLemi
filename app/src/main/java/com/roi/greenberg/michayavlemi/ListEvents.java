fed62294daca017efec235dbddce26032d899124
EINAV\einavsa <einav.sasson@gmail.com>
2018-03-15 18:05:44 +0200

    update dialog new event

--- app/src/main/java/com/roi/greenberg/michayavlemi/ListEvents.java
+++ app/src/main/java/com/roi/greenberg/michayavlemi/ListEvents.java
@@ -1,10 +1,12 @@
 package com.roi.greenberg.michayavlemi;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class ListEvents extends AppCompatActivity {
@@ -22,6 +24,23 @@ public class ListEvents extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                dialogNewEvent();
+
+
+
+    }
+
+    public void dialogNewEvent(){
+
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(ListEvents.this);
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.dialog_new_event, null);
+        builder.setTitle("CREATE NEW EVENT: ");
+        builder.setView(v);
+
+        android.app.AlertDialog dialog = builder.create();
+        dialog.show();
             }
         });
     }
